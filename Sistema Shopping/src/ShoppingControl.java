0040d40c84a59b5a37df411211592083e55d2446
Vinicius Alves de Santana <55450201+ViniciusTAS@users.noreply.github.com>
2020-03-31 10:35:17 -0300

    Add files via upload

---
+++ Sistema Shopping/src/ShoppingControl.java
@@ -0,0 +1,13 @@
+
+public class ShoppingControl {
+
+   public static LojasV00 objLoja;
+    
+    public static void main(String[] args) {
+       
+       objLoja = new LojasV00 ();
+       LojasView appShoppping = new LojasView();
+        
+    }
+    
+}
